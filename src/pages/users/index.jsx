cd1b0adfddace91d2a03edf6eb28d50c6b6aa6cf
elmurodurinboev <elmurodorinboyev@gmail.com>
2024-12-25 01:39:06 +0500

    feat: add users table

---
+++ src/pages/users/index.jsx
@@ -0,0 +1,165 @@
+import {Layout} from "@/components/custom/layout.jsx";
+import ThemeSwitch from "@/components/theme-switch.jsx";
+import {UserNav} from "@/components/user-nav.jsx";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table.jsx";
+import {useMutation, useQuery} from "@tanstack/react-query";
+import {Skeleton} from "@/components/ui/skeleton.jsx";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem, DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu.jsx";
+import {Button} from "@/components/custom/button.jsx";
+import {DotsHorizontalIcon} from "@radix-ui/react-icons";
+import {useNavigate} from "react-router-dom";
+import DefaultImage from "@/components/custom/default-image.jsx";
+import CourierService from "@/services/user.service.js";
+
+const Index = () => {
+  const usersData = useQuery({
+    queryKey: ['getAllUsers'],
+    queryFn: CourierService.getAll
+  })
+
+
+  const navigate = useNavigate()
+
+  const handleDelete = () => {
+
+  }
+
+  return (
+    <Layout>
+      {/* ===== Top Heading ===== */}
+      <Layout.Header sticky>
+        <div className="ml-auto flex items-center space-x-4">
+          <ThemeSwitch/>
+          <UserNav/>
+        </div>
+      </Layout.Header>
+
+      <Layout.Body>
+        <div className="mb-2 flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Foydalanuvchilar</h2>
+          </div>
+          <div>
+            <Button
+              onClick={() => navigate("create")}
+            >
+              Foydalanuvchi qo`shish
+            </Button>
+          </div>
+        </div>
+        <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
+          {
+            !usersData.isLoading ? (
+              usersData && usersData.data && usersData.isSuccess && !usersData.isError && usersData.data.result &&  (
+                <div className="rounded-md border min-h-[500px]">
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead>
+                          Ismi va rasmi
+                        </TableHead>
+                        <TableHead>
+                          Telefon raqami
+                        </TableHead>
+                        <TableHead>
+                          Jinsi
+                        </TableHead>
+                        <TableHead>
+                          Rol
+                        </TableHead>
+                        <TableHead className={"text-end"}>
+                        </TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {
+                        usersData.data.result.results && usersData.data.result.results.length > 0 ? (
+                          usersData.data.result.results.map((user, index) => (
+                            <TableRow key={index} className={"bg-secondary"}>
+                              <TableCell className={"flex gap-2 items-center overflow-hidden"}>
+                                {user.image ? (
+                                  <img
+                                    src={user.image}
+                                    alt={"product_image"}
+                                    className={"w-[48px] h-[48px] rounded-md object-cover"}
+                                  />
+                                ) : (
+                                  <DefaultImage/>
+                                )}
+                                <span>{user.full_name ? user.full_name : "Nomalum"}</span>
+                              </TableCell>
+
+                              <TableCell>
+                                {
+                                  user.phone_number
+                                }
+                              </TableCell>
+                              <TableCell>
+                                {
+                                  user.gender ? user.gender : "Nomalum"
+                                }
+                              </TableCell>
+                              <TableCell>
+                                {
+                                  user.user_role ? user.user_role : "Nomalum"
+                                }
+                              </TableCell>
+                              <TableCell className={"text-end"}>
+                                <div className={"w-auto flex justify-end items-center"}>
+                                  <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                      <Button
+                                        variant="ghost"
+                                        className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+                                      >
+                                        <DotsHorizontalIcon className="h-4 w-4"/>
+                                        <span className="sr-only">Open menu</span>
+                                      </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent align="end" className="w-[160px]">
+                                      <DropdownMenuItem onClick={() => navigate(`update/${user.id}`)}>Edit</DropdownMenuItem>
+                                      <DropdownMenuSeparator/>
+                                      <DropdownMenuItem
+                                        onClick={() => handleDelete(user)}
+                                      >
+                                        Delete
+                                      </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                  </DropdownMenu>
+                                </div>
+                              </TableCell>
+                            </TableRow>
+                          ))
+
+                        ) : (
+                          <TableRow>
+                            <TableCell
+                              colSpan={6}
+                              className="h-24 text-center text-rose-500 font-medium"
+                            >
+                              No results.
+                            </TableCell>
+                          </TableRow>
+                        )
+                      }
+
+                    </TableBody>
+                  </Table>
+                </div>
+              )
+            ) : (
+              <Skeleton className={"rounded-md border h-[500px]"}/>
+            )
+          }
+        </div>
+      </Layout.Body>
+    </Layout>
+  )
+};
+
+export default Index;
